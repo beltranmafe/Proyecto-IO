97bf2f091d131e629c103d991a97229ad6c2a9ca
Maria Fernanda Velandia Beltran <maria.velandia@vasslatam.com>
2020-03-25 12:07:48 -0500

    Se agrega estilo a la página de inicio.

--- Frontend/IOFrontend/src/app/inicio/components/inicio/inicio.component.ts
+++ Frontend/IOFrontend/src/app/inicio/components/inicio/inicio.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.component.html',
-  styleUrls: ['./inicio.component.css', '../../../app.component.css']
+  styleUrls: ['../../../app.component.css', './inicio.component.css']
 })
 export class InicioComponent implements OnInit {
 
